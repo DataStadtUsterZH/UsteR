58ce774721ef98a4969e2228067595ba6e167833
socialthink <78662247+socialthink@users.noreply.github.com>
2022-05-16 23:12:54 +0200

    update

---
+++ R/color.R
@@ -0,0 +1,42 @@
+
+#' Farb-Schemas gemäss CD der Stadt Uster.
+#'
+#' @param schema Die Bezeichnung des Schemas:
+#' @return Das Farbschema als Vector
+#' add(1, 1)
+#' @export
+usteR_load_color <- function() {
+
+
+  # Farben definieren
+
+  farbe_dunkelgruen <- rgb(63, 103, 68,191, max=255)
+  farbe_hellgruen <- rgb(165, 190, 55,191, max=255)
+  farbe_blaugruen <- rgb(83, 150, 135,191, max=255)
+  farbe_dunkelblau <- rgb(0, 95, 148,191, max=255)
+  farbe_himmelblau <- rgb(187, 209, 229,191, max=255)
+  farbe_tieforange <- rgb(213, 137, 37,191, max=255)
+  farbe_sonnengelb <- rgb(232, 209, 34,191, max=255)
+  farbe_purpur <- rgb(130, 40, 89,191, max=255)
+
+  farbe_dunkelgruen <<- rgb(63, 103, 68,191, max=255)
+  farbe_hellgruen <<- rgb(165, 190, 55,191, max=255)
+  farbe_blaugruen <<- rgb(83, 150, 135,191, max=255)
+  farbe_dunkelblau <<- rgb(0, 95, 148,191, max=255)
+  farbe_himmelblau <<- rgb(187, 209, 229,191, max=255)
+  farbe_tieforange <<- rgb(213, 137, 37,191, max=255)
+  farbe_sonnengelb <<- rgb(232, 209, 34,191, max=255)
+  farbe_purpur <<- rgb(130, 40, 89,191, max=255)
+
+  farbe_3gruen <<- c(farbe_dunkelgruen,farbe_hellgruen,farbe_blaugruen)
+  farbe_4gruenblau <<- c(farbe_dunkelgruen,farbe_hellgruen,farbe_blaugruen,farbe_dunkelblau)
+  farbe_3hell <<- c(farbe_himmelblau, farbe_tieforange, farbe_sonnengelb)
+  farbe_4hell <<- c(farbe_hellgruen,farbe_himmelblau, farbe_tieforange, farbe_sonnengelb)
+  farbe_5dunkel <<- c(farbe_dunkelgruen,farbe_hellgruen,farbe_blaugruen,farbe_dunkelblau, farbe_purpur)
+  farbe_6 <<- c(farbe_blaugruen, farbe_dunkelblau,farbe_himmelblau,farbe_tieforange,farbe_sonnengelb,farbe_purpur)
+  farbe_7 <<- c(farbe_hellgruen,farbe_blaugruen, farbe_dunkelblau,farbe_himmelblau,farbe_tieforange,farbe_sonnengelb,farbe_purpur)
+  farbe_8 <<- c(farbe_dunkelgruen,farbe_hellgruen,farbe_blaugruen, farbe_dunkelblau,farbe_himmelblau,farbe_tieforange,farbe_sonnengelb,farbe_purpur)
+  farbe_4zweipoole <<- c(farbe_dunkelgruen,farbe_hellgruen, farbe_sonnengelb, farbe_tieforange)
+  farbe_5angebote <<- c(farbe_tieforange,farbe_hellgruen,farbe_blaugruen,farbe_dunkelblau, farbe_purpur)
+
+ }
--- R/dataset.R
+++ R/dataset.R
@@ -1,10 +1,10 @@
 #' Verzeichnis von Datensätzen
 #'
-#' @return Ein Verzeichnis der Verfügbaren Daten. Bei stadtinterner Nutzung sind auch (noch) nicht öffentliche Daten verfügbar.
+#' @return Ein Verzeichnis der Verfügbaren Daten. Bei interner Nutzung in der Stadt sind auch (noch) nicht veröffentlichte Daten verfügbare.
 #' add(1, 1)
 #' @export
 
-uster_data_directory <- function() {
+usteR_data_directory <- function() {
   if(dir.exists("S:\\Arbeitsbereiche\\Open-Data-und-Statistik")){
     if(file.exists("S:/Arbeitsbereiche/Open-Data-und-Statistik/Data/verzeichnis.csv")){
       directory <- read.csv("S:\\Arbeitsbereiche\\Open-Data-und-Statistik\\Data\\verzeichnis.csv")
@@ -18,12 +18,12 @@ uster_data_directory <- function() {
 
 #' Laden eines Datensatz anhand der ID
 #'
-#' @param ID ID des gewünschten Datensatz
+#' @param id ID des gewünschten Datensatz
 #' @return Ein Dataset
 #' add(1, 1)
 #' @export
 
-uster_load_data <- function(id) {
+usteR_load_data <- function(id) {
   if(dir.exists("S:\\Arbeitsbereiche\\Open-Data-und-Statistik")){
     if(file.exists("S:/Arbeitsbereiche/Open-Data-und-Statistik/Data/verzeichnis.csv")){
       directory <- read.csv("S:\\Arbeitsbereiche\\Open-Data-und-Statistik\\Data\\verzeichnis.csv")
@@ -32,7 +32,7 @@ uster_load_data <- function(id) {
     directory <- read.csv("https://raw.githubusercontent.com/DataStadtUsterZH/DataDirectory/main/verzeichnis.csv")
   }
 
-  directory <- directory[directory$ID==id]
+  directory<- dplyr::filter(directory, ID==id)
   data <- read.csv(as.character(directory$URL))
   print(data)
    }
